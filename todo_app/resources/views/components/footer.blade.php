d7effa2bdc60331ab235188ce6fac65a0a8ccd2a
mattuntun <mattuntun3@gmail.com>
2020-03-22 18:26:55 +0900

    build js/css

---
+++ todo_app/resources/views/components/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="fixw-bottom bg-light d-flex flex-column justufy-xontent-endalign-items-end">
+    <div class="text-muted small px-3">
+        TODO App by Laravel & Bootstrap 4
+    </div>
+</footer>
